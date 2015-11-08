a74126dd7e93e1d3a52c8595ea3fb6fd973ac384
Caroline Whitman <calw001@bucknell.edu>
2015-11-08 15:48:21 -0500

    initial setup

---
+++ src/Scrabble/model/Tile.java
@@ -0,0 +1,24 @@
+/* *****************************************
+ * CSCI205 - Software Engineering and Design
+ * Fall 2015
+ *
+ * Name: NAMES of team members
+ * Date: Nov 8, 2015
+ * Time: 3:47:49 PM
+ *
+ * Project: csci205FinalProject
+ * Package: Scrabble.model
+ * File: Tile
+ * Description:
+ *
+ * ****************************************
+ */
+package Scrabble.model;
+
+/**
+ *
+ * @author calw001
+ */
+public class Tile {
+
+}
