9b9cc2829e9f43b600e250968dc55faf6dbb3457
Caroline Whitman <calw001@bucknell.edu>
2015-11-08 16:00:39 -0500

    Tile class

--- src/Scrabble/model/Tile.java
+++ src/Scrabble/model/Tile.java
@@ -19,6 +19,30 @@ package Scrabble.model;
  *
  * @author calw001
  */
+enum Val {
+    A(1), B(3), C(3), D(2), E(1), F(4), G(2), H(4), I(1), J(8), K(5), L(1), M(3), N(
+            1), O(1), P(3), Q(10), R(1), S(1), T(1), U(1), V(4), W(4), X(8), Y(4), Z(
+                    10);
+
+    private int score;
+
+    Val(int score) {
+        this.score = score;
+    }
+
+    int getScore() {
+        return this.score;
+    }
+}
+
 public class Tile {
+
+    private Val letter;
+    private int score;
+
+    public Tile(Val letter, int score) {
+        this.letter = letter;
+        this.score = score;
+    }
 
 }
