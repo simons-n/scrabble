b7bf59777d3e3d3a3c51b13852b00cdf80af59ac
Caroline Whitman <calw001@bucknell.edu>
2015-11-11 10:37:42 -0500

    progress

--- src/Scrabble/model/TileBag.java
+++ src/Scrabble/model/TileBag.java
@@ -28,106 +28,106 @@ public class TileBag {
 
     public TileBag(ArrayList tilesInBag) {
         this.tilesInBag = tilesInBag;
-        this.tilesInBag.add(new Tile(Val.A)); //*9
-        this.tilesInBag.add(new Tile(Val.A));
-        this.tilesInBag.add(new Tile(Val.A));
-        this.tilesInBag.add(new Tile(Val.A));
-        this.tilesInBag.add(new Tile(Val.A));
-        this.tilesInBag.add(new Tile(Val.A));
-        this.tilesInBag.add(new Tile(Val.A));
-        this.tilesInBag.add(new Tile(Val.A));
-        this.tilesInBag.add(new Tile(Val.A));
-        this.tilesInBag.add(new Tile(Val.B)); //*2
-        this.tilesInBag.add(new Tile(Val.B));
-        this.tilesInBag.add(new Tile(Val.C)); //*2
-        this.tilesInBag.add(new Tile(Val.C));
-        this.tilesInBag.add(new Tile(Val.D)); //*4
-        this.tilesInBag.add(new Tile(Val.D));
-        this.tilesInBag.add(new Tile(Val.D));
-        this.tilesInBag.add(new Tile(Val.D));
-        this.tilesInBag.add(new Tile(Val.E)); //*12
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.E));
-        this.tilesInBag.add(new Tile(Val.F)); //*2
-        this.tilesInBag.add(new Tile(Val.F));
-        this.tilesInBag.add(new Tile(Val.G)); //*3
-        this.tilesInBag.add(new Tile(Val.G));
-        this.tilesInBag.add(new Tile(Val.G));
-        this.tilesInBag.add(new Tile(Val.H)); //*2
-        this.tilesInBag.add(new Tile(Val.H));
-        this.tilesInBag.add(new Tile(Val.I)); //*9
-        this.tilesInBag.add(new Tile(Val.I));
-        this.tilesInBag.add(new Tile(Val.I));
-        this.tilesInBag.add(new Tile(Val.I));
-        this.tilesInBag.add(new Tile(Val.I));
-        this.tilesInBag.add(new Tile(Val.I));
-        this.tilesInBag.add(new Tile(Val.I));
-        this.tilesInBag.add(new Tile(Val.I));
-        this.tilesInBag.add(new Tile(Val.I));
-        this.tilesInBag.add(new Tile(Val.J)); //*1
-        this.tilesInBag.add(new Tile(Val.K)); //*1
-        this.tilesInBag.add(new Tile(Val.L)); //*4
-        this.tilesInBag.add(new Tile(Val.L));
-        this.tilesInBag.add(new Tile(Val.L));
-        this.tilesInBag.add(new Tile(Val.L));
-        this.tilesInBag.add(new Tile(Val.M)); //*2
-        this.tilesInBag.add(new Tile(Val.M));
-        this.tilesInBag.add(new Tile(Val.N)); //*6
-        this.tilesInBag.add(new Tile(Val.N));
-        this.tilesInBag.add(new Tile(Val.N));
-        this.tilesInBag.add(new Tile(Val.N));
-        this.tilesInBag.add(new Tile(Val.N));
-        this.tilesInBag.add(new Tile(Val.N));
-        this.tilesInBag.add(new Tile(Val.O)); //*8
-        this.tilesInBag.add(new Tile(Val.O));
-        this.tilesInBag.add(new Tile(Val.O));
-        this.tilesInBag.add(new Tile(Val.O));
-        this.tilesInBag.add(new Tile(Val.O));
-        this.tilesInBag.add(new Tile(Val.O));
-        this.tilesInBag.add(new Tile(Val.O));
-        this.tilesInBag.add(new Tile(Val.O));
-        this.tilesInBag.add(new Tile(Val.P)); //*2
-        this.tilesInBag.add(new Tile(Val.P));
-        this.tilesInBag.add(new Tile(Val.Q)); //*1
-        this.tilesInBag.add(new Tile(Val.R)); //*6
-        this.tilesInBag.add(new Tile(Val.R));
-        this.tilesInBag.add(new Tile(Val.R));
-        this.tilesInBag.add(new Tile(Val.R));
-        this.tilesInBag.add(new Tile(Val.R));
-        this.tilesInBag.add(new Tile(Val.R));
-        this.tilesInBag.add(new Tile(Val.S)); //*4
-        this.tilesInBag.add(new Tile(Val.S));
-        this.tilesInBag.add(new Tile(Val.S));
-        this.tilesInBag.add(new Tile(Val.S));
-        this.tilesInBag.add(new Tile(Val.T)); //*6
-        this.tilesInBag.add(new Tile(Val.T));
-        this.tilesInBag.add(new Tile(Val.T));
-        this.tilesInBag.add(new Tile(Val.T));
-        this.tilesInBag.add(new Tile(Val.T));
-        this.tilesInBag.add(new Tile(Val.T));
-        this.tilesInBag.add(new Tile(Val.U)); //*4
-        this.tilesInBag.add(new Tile(Val.U));
-        this.tilesInBag.add(new Tile(Val.U));
-        this.tilesInBag.add(new Tile(Val.U));
-        this.tilesInBag.add(new Tile(Val.V)); //*2
-        this.tilesInBag.add(new Tile(Val.V));
-        this.tilesInBag.add(new Tile(Val.W)); //*2
-        this.tilesInBag.add(new Tile(Val.W));
-        this.tilesInBag.add(new Tile(Val.X)); //*1
-        this.tilesInBag.add(new Tile(Val.Y)); //*2
-        this.tilesInBag.add(new Tile(Val.Y));
-        this.tilesInBag.add(new Tile(Val.Z)); //*1
-        this.tilesInBag.add(new Tile(Val.BLANK));//*2
-        this.tilesInBag.add(new Tile(Val.BLANK));
+
+        // 9 A tiles
+        for (int i = 0; i < 9; i++) {
+            this.tilesInBag.add(new Tile(Val.A));
+        }
+        // 2 B tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.B));
+        }
+        // 2 C tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.C));
+        }
+        // 4 D tiles
+        for (int i = 0; i < 4; i++) {
+            this.tilesInBag.add(new Tile(Val.D));
+        }
+        // 12 E tiles
+        for (int i = 0; i < 12; i++) {
+            this.tilesInBag.add(new Tile(Val.E));
+        }
+        // 2 F tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.F));
+        }
+        // 3 G tiles
+        for (int i = 0; i < 3; i++) {
+            this.tilesInBag.add(new Tile(Val.G));
+        }
+        // 2 H tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.H));
+        }
+        // 9 I tiles
+        for (int i = 0; i < 9; i++) {
+            this.tilesInBag.add(new Tile(Val.I));
+        }
+        // 1 J tile
+        this.tilesInBag.add(new Tile(Val.J));
+        // 1 K tile
+        this.tilesInBag.add(new Tile(Val.K));
+        // 4 L tiles
+        for (int i = 0; i < 4; i++) {
+            this.tilesInBag.add(new Tile(Val.L));
+        }
+        // 2 M tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.M));
+        }
+        // 6 N tiles
+        for (int i = 0; i < 6; i++) {
+            this.tilesInBag.add(new Tile(Val.N));
+        }
+        // 8 O tiles
+        for (int i = 0; i < 8; i++) {
+            this.tilesInBag.add(new Tile(Val.O));
+        }
+        // 2 P tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.P));
+        }
+        // 1 Q tile
+        this.tilesInBag.add(new Tile(Val.Q));
+        // 6 R tiles
+        for (int i = 0; i < 6; i++) {
+            this.tilesInBag.add(new Tile(Val.R));
+        }
+        // 4 S tiles
+        for (int i = 0; i < 4; i++) {
+            this.tilesInBag.add(new Tile(Val.S));
+        }
+        // 6 T tiles
+        for (int i = 0; i < 6; i++) {
+            this.tilesInBag.add(new Tile(Val.T));
+        }
+        // 4 U tiles
+        for (int i = 0; i < 4; i++) {
+            this.tilesInBag.add(new Tile(Val.U));
+        }
+        // 2 V tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.V));
+        }
+        // 2 W tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.W));
+        }
+        // 1 X tile
+        this.tilesInBag.add(new Tile(Val.X));
+        // 2 Y tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.Y));
+        }
+        // 1 Z tile
+        this.tilesInBag.add(new Tile(Val.Z));
+        // 2 BlANK tiles
+        for (int i = 0; i < 2; i++) {
+            this.tilesInBag.add(new Tile(Val.BLANK));
+        }
+
     }
 
     public void setTilesInBag(List<Tile> tilesInBag) {
