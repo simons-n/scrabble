c9577e61076ea26b7c0a5676dd22c4bdf73ee9b4
Jenna Slusar <jms107@bucknell.edu>
2015-11-12 21:45:23 -0500

    board squares

--- src/Scrabble/view/Board.java
+++ src/Scrabble/view/Board.java
@@ -26,22 +26,74 @@ import javax.swing.JLabel;
  */
 public class Board extends javax.swing.JPanel {
     private JLabel[][] grid;
+    private JLabel blankBoardSquare;
+    private JLabel doubleLetterScoreSquare;
+    private JLabel doubleWordScoreSquare;
+    private JLabel tripleLetterScoreSquare;
+    private JLabel tripleWordScoreSquare;
+    private JLabel starCenterSquare;
 
     public Board() {
         this.grid = new JLabel[15][15];
         setLayout(new GridLayout(15, 15));
         javax.swing.border.Border grayBorder = BorderFactory.createLineBorder(
-                Color.GRAY);
+                Color.WHITE);
         setBorder(grayBorder);
+        blankBoardSquare.setIcon(new javax.swing.ImageIcon(
+                getClass().getResources(
+                        "/csci205FinalProject/src/Scrabble/view/BackgroundSquare.png")));
         for (int x = 0; x < 15; x++) {
             for (int y = 0; x < 15; y++) {
-                this.grid[x][y] = new JLabel();
-                this.grid[x][y].setBackground(Color.WHITE);
+                if ((x == 0 & y == 0) || (x == 0 & y == 14) || (x == 14 & y == 0) || (x == 14 & y == 14) || (x == 14 & y == 7) || (x == 7 & y == 14) || (x == 0 & y == 7) || (x == 7 & y == 7)) {
+                    addTWS(this.grid, x, y);
+                } else if ((x == 7 & y == 7)) {
+                    addStar(this.grid, x, y);
+                } else if ((x != 0 & x != 14) & (y != 0 & y != 14) & y == x) {
+
+                }
+
+                this.grid[x][y] = blankBoardSquare;
                 this.grid[x][y].setBorder(BorderFactory.createLineBorder(
-                        Color.GRAY));
+                        Color.WHITE));
                 add(this.grid[x][y]);
             }
         }
+    }
+
+    public void addTWS(JLabel[][] grid, int x, int y) {
+        tripleWordScoreSquare.setIcon(new javax.swing.ImageIcon(
+                getClass().getResources(
+                        "/csci205FinalProject/src/Scrabble/view/tws.png")));
+        grid[x][y] = tripleWordScoreSquare;
+
+    }
+
+    public void addTLS(JLabel[][] grid, int x, int y) {
+        tripleLetterScoreSquare.setIcon(new javax.swing.ImageIcon(
+                getClass().getResources(
+                        "/csci205FinalProject/src/Scrabble/view/tls.png")));
+        grid[x][y] = tripleLetterScoreSquare;
+    }
+
+    public void addDWS(JLabel[][] grid, int x, int y) {
+        doubleWordScoreSquare.setIcon(new javax.swing.ImageIcon(
+                getClass().getResources(
+                        "/csci205FinalProject/src/Scrabble/view/dws.png")));
+        grid[x][y] = doubleWordScoreSquare;
+    }
+
+    public void addDLS(JLabel[][] grid, int x, int y) {
+        doubleLetterScoreSquare.setIcon(new javax.swing.ImageIcon(
+                getClass().getResources(
+                        "/csci205FinalProject/src/Scrabble/view/dls.png")));
+        grid[x][y] = doubleLetterScoreSquare;
+    }
+
+    public void addStar(JLabel[][] grid, int x, int y) {
+        starCenterSquare.setIcon(new javax.swing.ImageIcon(
+                getClass().getResources(
+                        "/csci205FinalProject/src/Scrabble/view/centerStar.png")));
+        grid[x][y] = doubleLetterScoreSquare;
     }
 
 }
