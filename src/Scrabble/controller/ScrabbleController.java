ff48de833de8cdad6cfc01815ea01c953dfbe124
Jenna Slusar <jms107@bucknell.edu>
2015-12-02 13:51:48 -0500

    fixing merge conflict

--- src/Scrabble/controller/ScrabbleController.java
+++ src/Scrabble/controller/ScrabbleController.java
@@ -80,7 +80,7 @@ public class ScrabbleController implements ActionListener {
     public void updateViewFromModel() {
         //this only updates the board not hand
         // when shuffle is pressed change HandView to HandView(Hand myhand)
-        handView.setHand(hand);
+        handView.setHand(hand); //repaint
         // when pass is pressed change view to next players hand
         // when swap is pressed change HandView to HandView(Hand myhand) *** make the dialog box show up
         // when play is pressed if not valid show error message, if valid update show message of how much they just scored, update total score, show new hand with drawn tiles, change player to next players hand
